eab4fe90598ddef658304db3fe5361827510d959
Skiba Filip <filip.skiba@ad.ms.gov.pl>
2020-01-15 13:00:46 +0100

    Cleanup

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,7 +1,5 @@
 package main;
 
-import com.itextpdf.text.FontFactory;
-import com.zeonpad.pdfcovertor.OutlookToPdf;
 
 import javax.swing.*;
 import java.io.File;
@@ -30,38 +28,21 @@ public class App {
         System.out.println(outDir);
         System.out.println();
 
-        OutlookToPdf outlookToPdf = new OutlookToPdf();
-        Attachment attachment = new Attachment();
         ArrayList<String> msgFiles = Arrays.stream(Objects.requireNonNull(file.list()))
                 .filter(v -> v.endsWith(".msg"))
                 .collect(Collectors.toCollection(ArrayList::new));
 
         for (String msgFile : msgFiles) {
 
-
             System.out.println("====== Extracting: " + msgFile + " ======");
-            outDir = file.getAbsolutePath() + "\\PDF\\" + msgFile.replace(".msg", "");
-            File dir = new File(outDir);
-            if (!dir.exists()) {
-                if (!dir.mkdirs()) {
-                    System.out.println("Błąd przy tworzeniu folderu wyjsciowego");
-                }
-            }
-            outlookToPdf.convert(inDir  + msgFile, outDir + msgFile.replace(".msg", ".pdf"));
 
-            Message message = new Message(dir + msgFile);
+            Message message = new Message(inDir, msgFile);
             message.processMessage();
 
-            attachment.extract(inDir, outDir, msgFile);
             System.out.println(outDir);
             System.out.println("====== Done! ======");
         }
         System.out.println("Extraction Complete.");
-       /* try {
-            Message processor = new Message(inDir+"\\20180922_133204_1fa3e2d2-8adc-4526-a8b8-5d6acd22fc38.msg");
-            processor.processMessage();
-        } catch (IOException e) {
-            System.err.println("Could not process " + inDir + ": " + e);
-        }*/
+
     }
 }
