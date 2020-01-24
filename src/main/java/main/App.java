35e21e21d1d84b46c24e170a718c1162608cc83a
Skiba Filip <filip.skiba@ad.ms.gov.pl>
2020-01-24 15:57:32 +0100

    Overhoul of Application, Switching to JAVA FX

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,48 +1,23 @@
 package main;
 
 
-import javax.swing.*;
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 
 public class App {
-    public static void main(String[] args) throws IOException {
 
-        //FontFactory.getRegisteredFonts().forEach(System.out::println);
-        JFileChooser f = new JFileChooser();
-        f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        f.setDialogTitle("Choose folder with MSG files.");
-        f.showOpenDialog(null);
-
-
-        File file = f.getSelectedFile();
-
-        String inDir = file.getAbsolutePath()+"\\";
-        String outDir = file.getAbsolutePath() + "\\PDF\\";
-        System.out.println(inDir);
-        System.out.println(outDir);
-        System.out.println();
-
-        ArrayList<String> msgFiles = Arrays.stream(Objects.requireNonNull(file.list()))
-                .filter(v -> v.endsWith(".msg"))
-                .collect(Collectors.toCollection(ArrayList::new));
-
-        for (String msgFile : msgFiles) {
-
-            System.out.println("====== Extracting: " + msgFile + " ======");
-
-            Message message = new Message(inDir, msgFile);
-            message.processMessage();
-
-            System.out.println(outDir);
-            System.out.println("====== Done! ======");
-        }
-        System.out.println("Extraction Complete.");
+    static {
+            try {
+                NativeUtils.loadLibraryFromJar("/jacob-1.18-x64.dll"); // during runtime. .DLL within .JAR
+            } catch (IOException e1) {
+                throw new RuntimeException(e1);
+            }
 
     }
+    public static void main(String[] args) throws IOException {
+
+        DirectoryChoose.main(args);
+
+    }
+
 }
--- src/main/java/main/DirectoryChoose.java
+++ src/main/java/main/DirectoryChoose.java
@@ -1,35 +1,147 @@
 package main;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
-
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class DirectoryChoose extends Application {
+
+    private File selectedDirectory;
+    private DirectoryChooser directoryChooser = new DirectoryChooser();
+    private Label pathLabel = new Label();
+    private TextArea textArea = new TextArea();
+    private Image image = new Image(DirectoryChoose.class.getResourceAsStream("/icon.png"));
+    private ImageView imageView = new ImageView(image);
+    private Button button = new Button("Select Directory");
+    private Button convertButton = new Button("Start", imageView);
+    private ArrayList<String> msgFiles = new ArrayList<>();
+    private ProgressBar progressBar = new ProgressBar();
+
+    private static int BUTTON_HEIGHT = 28;
+    private static int BUTTON_WIDTH = 80;
+
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("JavaFX App");
+        primaryStage.setTitle("MTP Converter");
+        primaryStage.getIcons().add(image);
 
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        directoryChooser.setInitialDirectory(new File("src"));
+        imageView.setFitHeight(15);
+        imageView.setPreserveRatio(true);
+        button.setMinSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        convertButton.setMinSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        pathLabel.setText("Please choose directory");
+        textArea.setMinHeight(80);
+        textArea.setEditable(false);
 
-        Button button = new Button("Select Directory");
-        button.setOnAction(e -> {
-            File selectedDirectory = directoryChooser.showDialog(primaryStage);
 
-            System.out.println(selectedDirectory.getAbsolutePath());
+        progressBar.setPrefWidth(350);
+        Button cancelButton = new Button("Close");
+        cancelButton.setOnAction(event -> System.exit(1));
+        cancelButton.setMinSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+
+        button.setOnAction(event -> {
+            selectedDirectory = directoryChooser.showDialog(primaryStage);
+            if (selectedDirectory != null) {
+                pathLabel.setText(selectedDirectory.getAbsolutePath());
+
+                msgFiles = Arrays.stream(Objects.requireNonNull(selectedDirectory.list()))
+                        .filter(v -> v.endsWith(".msg"))
+                        .collect(Collectors.toCollection(ArrayList::new));
+
+                if (msgFiles.isEmpty()) {
+                    textArea.appendText("No msg files in given location!");
+                    selectedDirectory = null;
+                } else {
+                    textArea.setText("Found " + msgFiles.size() + " files");
+                    textArea.appendText("Program may ask for permission from Outlook. Give Access or it won't work:) ");
+                }
+            }
+        });
+        convertButton.setOnAction(e -> {
+            if (selectedDirectory == null) {
+                textArea.setText("No directory chosen!\n");
+            } else {
+                executeFiles();
+
+            }
         });
 
 
-        VBox vBox = new VBox(button);
+        HBox hBox = new HBox(button, convertButton);
+        hBox.setSpacing(5);
+        hBox.setPadding(new Insets(10));
+        hBox.setAlignment(Pos.CENTER);
+        VBox vBox = new VBox(pathLabel, hBox, progressBar, textArea, cancelButton);
+        vBox.setSpacing(5);
+        vBox.setPadding(new Insets(10));
+        vBox.setAlignment(Pos.CENTER);
         //HBox hBox = new HBox(button1, button2);
-        Scene scene = new Scene(vBox, 960, 600);
+        Scene scene = new Scene(vBox, 600, 400);
+        primaryStage.setMaxWidth(600);
+        primaryStage.setMinWidth(400);
+        primaryStage.setMaxHeight(400);
+        primaryStage.setMinHeight(300);
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
+
+    private void executeFiles(){
+        Thread thread = new Thread(()->{
+            button.setDisable(true);
+            convertButton.setDisable(true);
+            String inDir = selectedDirectory.getAbsolutePath() + "\\";
+            String outDir = selectedDirectory.getAbsolutePath() + "\\PDF\\";
+            Platform.runLater(() -> progressBar.setProgress(0d));
+            int counter = 1;
+
+            for (String msgFile : msgFiles) {
+                textArea.appendText("\nExtracting: " + msgFile + "\nto: " + outDir);
+
+                Message message = null;
+                try {
+                    message = new Message(inDir, msgFile);
+                    message.processMessage();
+
+                } catch (IOException ex) {
+                    textArea.appendText(ex.toString());
+                }
+
+                textArea.appendText("\nDone!\n");
+                progressBar.setProgress(counter/(double)msgFiles.size());
+                counter++;
+
+            }
+            textArea.appendText("Extraction Complete!");
+            button.setDisable(false);
+            convertButton.setDisable(false);
+
+        });
+        thread.start();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
+
